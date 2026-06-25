1899cc252a082b0a56c5f39df23124dbec9deb31
renanneryn <renanneryn@gmail.com>
2026-06-25 13:20:58 -0300

    chore: forcar redeploy do admin atualizado

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+const ADMIN_SYNC_BUILD = '2026-06-25-admin-auth-proxy';
+void ADMIN_SYNC_BUILD;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
